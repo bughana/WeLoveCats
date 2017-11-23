777e95da4b820b6cdebe6d3b1fe9749d2d867799
hannahteuteberg <hannah.teuteberg@gmx.de>
2017-11-23 20:33:13 +0100

    MOD: update kingfisher to swift 4 version + add assets

--- WeLoveCats/WeLoveCats/UI/List/CatListCell.swift
+++ WeLoveCats/WeLoveCats/UI/List/CatListCell.swift
@@ -1,8 +1,30 @@
 
 import UIKit
+import Cartography
 
 class CatListCell: UICollectionViewCell {
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(imageView)
+        addSubview(likeButton)
+        constrainSubviews()
+    }
+    
+    // MARK: - Private: Autolayout
+    private func constrainSubviews() {
+        constrain(imageView, self) { imageView, superview in
+            imageView.edges == superview.edges
+        }
+        
+        constrain(likeButton, self) { likeButton, superview in
+            likeButton.right == superview.right - 10
+            likeButton.bottom == superview.bottom - 10
+        }
+    }
+    
+    // MARK: - Subviews
     private lazy var imageView: UIImageView = {
         let imageView = UIImageView()
         
@@ -12,6 +34,13 @@ class CatListCell: UICollectionViewCell {
     private lazy var likeButton: UIButton = {
         let likeButton = UIButton()
         
+        likeButton.setImage(UIImage(named: "like"), for: .normal)
+        likeButton.setImage(UIImage(named: "likeFilled"), for: .selected)
+        
         return likeButton
     }()
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
